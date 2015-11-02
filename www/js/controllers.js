153206e3f980151f1a529ef1bf4623a348e8a36b
Tom Blaymire <Admin@toms-mbp.home>
2015-11-02 23:28:08 +0000

    Added google maps api controller and view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,24 +13,62 @@ angular.module('starter.controllers', ['starter.services'])
 
 })
 
-/*
-
-exampleApp.controller('SaveBandCtrl', function($scope) {
-
-	$scope.savedData = function(v) {
-		window.localStorage.setItem("data", v); 
-
-	}
-
-	$scope.loadData = function() {
-		alert(window.localStorage.getItem("data")); 
-	}
-
+.controller('BandsseenCtrl', function($scope, $ionicModal){
+$ionicModal.fromTemplateUrl('addnewband.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  })
 })
 
-)
 
-*/
+
+// A Controller To Display A Google Map Of The Current Location 
+.controller('MapCtrl', function($scope, $ionicLoading) {
+
+    google.maps.event.addDomListener(window, 'load', function() {
+        var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+ 
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+ 
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            var myLocation = new google.maps.Marker({
+                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                map: map,
+                title: "My Location"
+            });
+        });
+ 
+        $scope.map = map;
+    })
+ 
+})
 
 
 .controller('CountdownCtrl', function($scope) {
